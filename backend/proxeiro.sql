e7179df4fedbb76d714b71fc9e5e55f8c40b98c4
ntua-el19913 <el19913@mail.ntua.gr>
2024-04-17 18:51:16 +0300

    dummy ratings

--- backend/proxeiro.sql
+++ backend/proxeiro.sql
@@ -3,3 +3,7 @@ SELECT * FROM Recipe;
 INSERT INTO cooks_in_recipe (chef_id,rec_id) VALUES (1,1);
 
 SELECT cooks.chef_id,_name,Count(rec_id) as total_recipes FROM cooks INNER JOIN cooks_in_recipe using (chef_id) WHERE cooks.age < 30 group by chef_id ORDER BY total_recipes DESC LIMIT 10;
+
+select * from recipe;
+
+SELECT nation,count(nation) from Recipe group by nation;
