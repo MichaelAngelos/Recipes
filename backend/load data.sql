fa0716b4ae2bc460dab26dbf625e000424a104b1
ntua-el19913 <el19913@mail.ntua.gr>
2024-04-17 17:40:57 +0300

    progress on dummy data and 3-3 answered

--- backend/load data.sql
+++ backend/load data.sql
@@ -1,3 +1,5 @@
+DELETE FROM Cooks_in_Recipe;
+DELETE FROM Cooks;
 DELETE FROM Recipe;
 
 LOAD DATA INFILE 'dummy_recipes.tsv'
@@ -7,3 +9,23 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 SELECT * FROM Recipe;
+
+#DELETE FROM Cooks;
+
+LOAD DATA INFILE 'dummy_cooks.tsv'
+INTO TABLE Cooks
+FIELDS TERMINATED BY '	'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM Cooks;
+
+#DELETE FROM Cooks_in_Recipe;
+
+LOAD DATA INFILE 'dummy_cooks_in_recipes.tsv'
+INTO TABLE cooks_in_recipe
+FIELDS TERMINATED BY '	'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM cooks_in_recipe;
