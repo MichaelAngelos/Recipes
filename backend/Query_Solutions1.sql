e60e281b77d15efd1992c0eec9ca08a9d8923f2c
ntua-el19913 <el19913@mail.ntua.gr>
2024-05-07 16:43:44 +0300

    query 3.3 solution

--- backend/Query_Solutions1.sql
+++ backend/Query_Solutions1.sql
@@ -8,7 +8,13 @@ SELECT * FROM Cooks where List_of_Specializations_in_Nations like '%American%'
 	and Years_of_Experience = 25 and chef_id IN (SELECT chef_id FROM episode_cooks);
 
 	#SELECT * FROM episode_cooks
+
+#Query 3.3
+
+SELECT cooks.*,COUNT(rec_id) as Number_of_Recipes FROM cooks inner join cooks_in_recipe using (chef_id)
+		where age < 30 GROUP BY chef_id ORDER BY Number_of_Recipes DESC LIMIT 3;
     
 #Query 3.4
 
 SELECT * FROM Cooks where chef_id NOT IN (Select chef_id from episode_judges);
+
